f300d416c2465b04f2cb5526bb09682d08ce0654
Konia Huo <huoqihua@cn.ibm.com>
2024-03-11 00:19:22 +0800

    update

--- admin/src/app/[locale]/(authentication)/layout.tsx
+++ admin/src/app/[locale]/(authentication)/layout.tsx
@@ -5,29 +5,18 @@ export default function AuthenticationLayout({ children }: { children: ReactNode
   return (
     <main className="relative grid h-full flex-col items-center justify-center bg-background lg:grid-cols-2 lg:px-0">
       <div className="relative hidden h-full flex-col p-6 text-white lg:flex">
-        <div className="absolute inset-0 bg-black ">
+        <div className="absolute inset-0 bg-black">
           <Image
-            src="/images/genshin-impact-logo.png"
+            src="/images/Genshin_Japan_Logo.svg"
             alt="logo"
-            width={256}
-            height={256}
-            className="absolute -top-5 left-3 z-10 h-auto w-36 invert"
+            width={80}
+            height={60}
+            className="absolute left-4 top-3 z-10 invert"
           />
-          <Image
-            src="/images/background.jpg"
-            width={1920}
-            height={1080}
-            alt="Authentication"
-            className="block h-full w-full object-cover opacity-65 dark:hidden"
-            priority
-          />
+          <video className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" autoPlay loop muted>
+            <source src="/images/GENSHIN IMPACT ELEMENTS 1.0.mp4" type="video/mp4" />
+          </video>
         </div>
-        <blockquote className="font-genshin relative mb-4 ml-auto mr-4 mt-auto w-[350px]">
-          <p className="text-right text-4xl font-semibold uppercase">
-            When the enemy tells you you&apos;re going to fail at something, do it anyway. nothing is more frightening
-            than never changing.
-          </p>
-        </blockquote>
       </div>
       {children}
     </main>
--- admin/src/app/[locale]/(authentication)/sign-in/page.tsx
+++ admin/src/app/[locale]/(authentication)/sign-in/page.tsx
@@ -69,9 +69,9 @@ export default function SignInPage() {
 
   return (
     <section className="p-8">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[450px]">
         <div className="flex flex-col space-y-2 text-center">
-          <h1 className="font-genshin text-2xl font-semibold tracking-tight">Hi, Welcome Back!</h1>
+          <h1 className="font-genshin text-2xl font-semibold tracking-tight">Hi, Welcome to Genshin Lab!</h1>
           {/* <p className="text-sm text-muted-foreground">Enter your email below to create your account</p> */}
         </div>
         <div className="grid gap-6">
